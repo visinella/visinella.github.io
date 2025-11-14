72053be630352a1041772767dd35983834f0616b
Victor D <victor@wsl.arch>
2025-11-14 14:15:32 +0200

    Init new astro project using template wpinfusion/AstroPie

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
+import playformInline from "@playform/inline";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	site: "https://astropie.netlify.app",
+	base: "/",
+	// trailingSlash: 'always',
+	integrations: [
+		alpinejs(),
+		playformInline({
+			Beasties: true,
+		}),
+		mdx(),
+	],
+	output: "static",
+	devToolbar: {
+		enabled: false,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
