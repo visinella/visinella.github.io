f8ff0292a7b0fd305b92ac47b46d159ee4982128
ava arch <victordumbrava91@gmail.com>
2025-12-23 22:23:31 +0200

    fixed typescript issue

--- astro/src/env.d.ts
+++ astro/src/env.d.ts
@@ -1,3 +1,27 @@
+/// <reference types="astro/client" />
+
 interface Window {
 	Alpine: import("alpinejs").Alpine;
+}
+
+declare global {
+	namespace astroHTML.JSX {
+		interface HTMLAttributes {
+			[key: `@${string}`]: string | undefined;
+			[key: `x-${string}`]: string | boolean | undefined;
+			[key: `x-transition:${string}`]: string | undefined;
+			_keydown?: any;
+			_click?: any;
+		}
+		interface ButtonHTMLAttributes {
+			[key: `@${string}`]: string | undefined;
+			[key: `x-${string}`]: string | boolean | undefined;
+			_click?: any;
+		}
+		interface AnchorHTMLAttributes {
+			[key: `@${string}`]: string | undefined;
+			[key: `x-${string}`]: string | boolean | undefined;
+			_click?: any;
+		}
+	}
 }
